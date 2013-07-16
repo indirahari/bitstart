5c81f908773cedafcf8dd88244612a9589453d2b
Indira <ubuntu@ip-172-31-34-143.us-west-2.compute.internal>
2013-07-16 13:17:12 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,34 +1,21 @@
-var express = require('express');
+
+s = require('express');
     fs = require("fs");
-
-// var buff = new Buffer(25);
-
-
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html','utf-8',function (err,buff))  {
+  fs.readFile('index.html','utf-8',function (err,data)  {
 	if (err) throw err;
-	response.send('error');
+	response.send(data);
 });
-
-//buf = new Buffer(256);
-//len = buf.write('\u00bd + \u00bc = \u00be', 0);
-//response.send(len + "hello indira " + buf.toString('utf8', 0, len));
-response.send('hello indira');
-
-
-
-
-
-
-  
+  response.send('data by indira');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+`
 
