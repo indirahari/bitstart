67f5791e7f9f57d1ce382b28f6421cb26c8c14ae
Indira <ubuntu@ip-172-31-34-143.us-west-2.compute.internal>
2013-07-16 11:21:43 +0000

    Updated with index file

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
+s = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html',function (err,data)  {
+	if (err) throw err;
+	response.send(data);
+});
+  response.send('data');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
